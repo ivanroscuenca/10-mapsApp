e7781ff538ea0945fc4d6532a4dff76ea90fd2f6
ivanroscuenca <ivanros@protonmail.com>
2023-09-20 12:45:04 +0200

    video305

--- src/app/alone/pages/alone-page/alone-page.component.ts
+++ src/app/alone/pages/alone-page/alone-page.component.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { CounterAloneComponent } from '../../components/counter-alone/counter-alone.component';
 
 @Component({
   standalone: true,
-  // imports: [CommonModule],
+  imports: [CounterAloneComponent],
   templateUrl: './alone-page.component.html',
   styleUrls: ['./alone-page.component.css'],
 })
